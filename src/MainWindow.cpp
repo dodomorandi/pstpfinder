0cddc98039af84f23d99b8c1372693f7513fec22
Edoardo Morandi <morandidodo@gmail.com>
2011-03-02 20:53:10 +0100

    Removed "this->" references.
    I really don't know if it could be a good idea, because using "this" avoids conflicting variables. However I shouldn't need it if I write a good code. Isn't it?

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -7,26 +7,26 @@ using namespace Gtk;
 
 MainWindow::MainWindow()
 {
-  this->init();
-  this->show_all();
+  init();
+  show_all();
 }
 
 void
 MainWindow::init()
 {
-  this->buttonNew.set_label("New analysis");
-  this->buttonNew.signal_clicked().
+  buttonNew.set_label("New analysis");
+  buttonNew.signal_clicked().
     connect(sigc::mem_fun(*this, &MainWindow::createNewAnalysis));
-  this->buttonOpen.set_label("Open analysis...");
+  buttonOpen.set_label("Open analysis...");
   
-  this->buttonBox.add((Widget&)this->buttonNew);
-  this->buttonBox.add(this->buttonOpen);
-  this->buttonBox.set_layout(BUTTONBOX_SPREAD);
-  this->buttonBox.set_spacing(10);
-  this->buttonBox.set_border_width(10);
-  this->buttonBox.set_child_min_height(40);
+  buttonBox.add((Widget&)buttonNew);
+  buttonBox.add(buttonOpen);
+  buttonBox.set_layout(BUTTONBOX_SPREAD);
+  buttonBox.set_spacing(10);
+  buttonBox.set_border_width(10);
+  buttonBox.set_child_min_height(40);
   
-  this->add(this->buttonBox);
+  add(buttonBox);
 }
 
 void
