7633537707ae76a0045b89187787c6f008912329
Edoardo Morandi <morandidodo@gmail.com>
2011-03-10 16:08:22 +0100

    Written a base for calculateSAS
    This is completely taken from gmx_sas.c. Lots of changes must be done. First of all a dynamic way to handle atom indexes must be written. Then we have to work on data saving. A hybrid disk/ram handling would be great.

--- include/Gromacs.h
+++ include/Gromacs.h
@@ -13,7 +13,17 @@ extern "C" {
 #include <tpxio.h>
 #include <mtop_util.h>
 #include <main.h>
+#include <rmpbc.h>
 };
+
+/* Taken from src/tools/nsc.h, Gromacs 4.5.2 */
+#define FLAG_ATOM_AREA  04
+extern "C" int nsc_dclm_pbc(rvec *coords, real *radius, int nat,
+                        int  densit, int mode,
+                        real *value_of_area, real **at_area,
+                        real *value_of_vol,
+                        real **lidots, int *nu_dots,
+                        atom_id index[],int ePBC,matrix box);
 
 using namespace std;
 
@@ -22,9 +32,11 @@ namespace Gromacs
   class Gromacs
   {
   public:
-    Gromacs();
+    Gromacs(float solventSize = 0.14);
     ~Gromacs();
-    Gromacs(const string& trajectoryFileName, const string& topologyFileName);
+    Gromacs(const string& trajectoryFileName,
+            const string& topologyFileName,
+            float solventSize = 0.14);
     // FIXME: It will have to return an object Molecular Dynamics with SAS
     // FIXME: additional informations
     bool calculateSas() const;
@@ -32,8 +44,9 @@ namespace Gromacs
   private:
     gmx_atomprop_t aps;
     string trjName, tprName;
+    float solSize;
     
-    void init();
+    void init(float solventSize);
   };
 };
 
--- src/Gromacs.cpp
+++ src/Gromacs.cpp
@@ -1,26 +1,28 @@
 #include "Gromacs.h"
 #include <string>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 namespace Gromacs
 {
-  Gromacs::Gromacs()
+  Gromacs::Gromacs(float solventSize)
   {
-    init();
+    init(solventSize);
   }
 
   Gromacs::Gromacs( const string& trajectoryFileName,
-                    const string& topologyFileName)
+                    const string& topologyFileName, 
+                    float solventSize)
   {
     trjName = trajectoryFileName;
     tprName = topologyFileName;
-    init();
+    init(solventSize);
   }
 
   void
-  Gromacs::init()
+  Gromacs::init(float solventSize)
   {
 #ifndef GMX45
     // FIXME: Test versions until 4.5
@@ -29,6 +31,7 @@ namespace Gromacs
     cr = init_par(&argc, &argv);
 #endif
     
+    solSize = solventSize;
     aps = gmx_atomprop_init();
   }
 
@@ -50,11 +53,22 @@ namespace Gromacs
     real t, lambda;
 #endif
 
-    int natoms;
+    bool bTop, bDGsol;
+    bool *bOut;
     t_inputrec ir;
     matrix box;
     gmx_mtop_t mtop;
-    t_trxframe fr;
+    matrix topbox;
+    rvec *xtop, *x;
+    real t, totarea, totvolume, tarea;
+    int natoms, ePBC, nsurfacedots;
+    t_topology top;
+    char title[1024];
+    real *dgs_factor, *radius, *area, *surfacedots, dgsolv;
+    atom_id* index[2];
+    gmx_rmpbc_t gpbc;
+    char* grpname[2];
+    int nx[2];
     
 #ifdef GMX45
     oenv = new output_env;
@@ -65,26 +79,89 @@ namespace Gromacs
               0, 0, 0, &mtop);
 #endif
     
+    bTop = read_tps_conf( tprName.c_str(), title, &top, &ePBC, &xtop, 
+                          NULL, topbox, FALSE);
+    
+    bDGsol = (strcmp(*(top.atoms.atomtype[0]),"?") != 0);
+    // TODO: warnings about bDGsol
+    
 #ifdef GMX45
-    if(!read_first_frame(oenv, &status, trjName.c_str(), &fr, TRX_NEED_X))
+    if((natoms = 
+          read_first_x(oenv, &status, trjName.c_str(), &t, &x, box)) == 0)
 #else
-    if(!read_first_frame(&status, trjName.c_str(), &fr, TRX_NEED_X))
+    if((natoms = 
+          read_first_x(&status, trjName.c_str(), &t, &x, box)) == 0)
 #endif
-      gmx_fatal(FARGS, "The trajectory file is invalid.\n");
+      gmx_fatal(FARGS, "Could not read coordinates from statusfile.\n");
+    
+    // TODO: index file handling and integration
+    get_index(&top.atoms, 0, 2, nx, index, grpname);
+    bOut = new bool[natoms];
+    for(int* i = index[1]; i < index[1] + nx[1]; i++)
+      bOut[*i] = TRUE;
+    
+    dgs_factor = new real[nx[0]];
+    radius = new real[natoms];
+    
+    for(int i = 0; i < natoms; i++)
+    {
+      gmx_atomprop_query( aps, epropVDW,
+                          *(top.atoms.resinfo[top.atoms.atom[i].resind].name),
+                          *(top.atoms.atomname[i]), radius + i);
+      radius[i] += solSize;
+    }
+    for(int i = 0; i < nx[0]; i++)
+    {
+      int ii = index[0][i];
+      if( !gmx_atomprop_query( aps, epropVDW,
+                          *(top.atoms.resinfo[top.atoms.atom[ii].resind].name),
+                          *(top.atoms.atomname[ii]), dgs_factor + i))
+        dgs_factor[i] = 0;
+    }
+    
+    gpbc = gmx_rmpbc_init(&top.idef, ePBC, natoms, box);
     
     do
     {
-      cout <<  endl << fr.natoms << " atoms read." << endl;
+      gmx_rmpbc(gpbc, natoms, box, x);
+      if(nsc_dclm_pbc(x, radius, nx[0], 24, FLAG_ATOM_AREA, &totarea,
+                            &area, &totvolume, &surfacedots, &nsurfacedots,
+                            index[0], ePBC, box) != 0)
+        gmx_fatal(FARGS, "Something wrong in nsc_dclm_pbc");
+        
+        tarea = 0;
+        dgsolv = 0;
+        for(int i = 0; i < nx[0]; i++)
+        {
+          if(bOut[index[0][i]])
+          {
+            tarea += area[i];
+            if(bDGsol)
+              dgsolv += area[i]*dgs_factor[i];
+          }
+        }
+      cout << "At " << t << " ps area is " << tarea << endl;
+      
+      delete[] area;
+      if(nsurfacedots > 0)
+        delete[] surfacedots;
+    }  
 #ifdef GMX45
-    } while(read_next_frame(oenv, status, &fr));
+    while(read_next_x(oenv, status, &t, natoms, x, box));
 #else
-    } while(read_next_frame(status, &fr));
+    while(read_next_x(status, &t, natoms, x, box));
 #endif
+
+    gmx_rmpbc_done(gpbc);
 
     close_trx(status);
 #ifdef GMX45    
     output_env_done(oenv);
 #endif
+
+    delete[] bOut;
+    delete[] dgs_factor;
+    delete[] radius;
 
     return true;
   }
