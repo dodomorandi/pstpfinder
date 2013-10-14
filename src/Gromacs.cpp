dodomorandi/pstpfinder

commit b63d29a7e3f8953f3dbad28ec336b204a7547c1c

Fixed lots of memory leaks

src/Gromacs.cpp
@@ -38,6 +38,7 @@
 #include <gromacs/do_fit.h>
 #include <gromacs/smalloc.h>
 #include <gromacs/vec.h>
+#include <gromacs/statutil.h>
 
 using namespace std;
 
@@ -76,10 +77,7 @@ namespace PstpFinder
     lambda = gromacs.lambda;
 #endif
     status = gromacs.status;
-    t = gromacs.t;
-    x = gromacs.x;
     xtop = gromacs.xtop;
-    memcpy(box, gromacs.box, sizeof(*box));
     natoms = gromacs.natoms;
     ePBC = gromacs.ePBC;
     if(gromacs.gotTopology)
@@ -159,6 +157,34 @@ namespace PstpFinder
     else
       aps = 0;
 
+    memcpy(&fr, &gromacs.fr, sizeof(t_trxframe));
+    if(gromacs.fr.title != nullptr)
+      fr.title = strdup(gromacs.fr.title);
+
+    if(gromacs.fr.atoms != nullptr)
+      fr.atoms = copy_t_atoms(gromacs.fr.atoms);
+
+    if(gromacs.fr.x != nullptr)
+    {
+      snew(fr.x, gromacs.fr.natoms);
+      memcpy(fr.x, gromacs.fr.x, sizeof(rvec) * gromacs.fr.natoms);
+    }
+
+    if(gromacs.fr.v != nullptr)
+    {
+      snew(fr.v, gromacs.fr.natoms);
+      memcpy(fr.v, gromacs.fr.v, sizeof(rvec) * gromacs.fr.natoms);
+    }
+
+    if(gromacs.fr.f != nullptr)
+    {
+      snew(fr.f, gromacs.fr.natoms);
+      memcpy(fr.f, gromacs.fr.f, sizeof(rvec) * gromacs.fr.natoms);
+    }
+
+    // FIXME: vmdplugin copy
+    memcpy(&fr.vmdplugin, &gromacs.fr.vmdplugin, sizeof(t_gmxvmdplugin));
+
     trjName = gromacs.trjName;
     tprName = gromacs.tprName;
     gotTrajectory = gromacs.gotTrajectory;
@@ -213,8 +239,11 @@ namespace PstpFinder
     if(gotTrajectory)
     {
       output_env_done(oenv);
-      close_trj(status);
+      close_trx(status);
     }
+
+    if(gotTopology)
+      sfree(xtop);
 #endif
     gmx_atomprop_destroy(aps);
   }
@@ -300,7 +329,7 @@ namespace PstpFinder
     }
 
     if(_usePBC)
-      gpbc = gmx_rmpbc_init(&top.idef, ePBC, natoms, box);
+      gpbc = gmx_rmpbc_init(&top.idef, ePBC, natoms, fr.box);
 
     SasAnalysis<Stream> sasAnalysis(nx, *this, session);
     {
@@ -325,16 +354,16 @@ namespace PstpFinder
         break;
       }
       if(_usePBC)
-        gmx_rmpbc(gpbc, natoms, box, x);
+        gmx_rmpbc(gpbc, natoms, fr.box, fr.x);
 
       int nsc_dclm_pdc_result;
       {
         unique_lock<mutex> lock(nsc_dclm_pbc_mutex);
-        nsc_dclm_pdc_result = nsc_dclm_pbc(x, radius, nx, 24, FLAG_ATOM_AREA,
+        nsc_dclm_pdc_result = nsc_dclm_pbc(fr.x, radius, nx, 24, FLAG_ATOM_AREA,
                                            &totarea, &area, &totvolume,
                                            &surfacedots, &nsurfacedots,
                                            index.data(), ePBC,
-                                           _usePBC ? box : nullptr);
+                                           _usePBC ? fr.box : nullptr);
       }
       if(nsc_dclm_pdc_result != 0)
         gmx_fatal(FARGS, "Something wrong in nsc_dclm_pbc");
@@ -343,9 +372,9 @@ namespace PstpFinder
       dgsolv = 0;
       for(int i = 0; i < nx; i++)
       {
-        atoms[i].x = x[index[i]][0];
-        atoms[i].y = x[index[i]][1];
-        atoms[i].z = x[index[i]][2];
+        atoms[i].x = fr.x[index[i]][0];
+        atoms[i].y = fr.x[index[i]][1];
+        atoms[i].z = fr.x[index[i]][2];
         atoms[i].sas = area[i];
 
         if(bDGsol)
@@ -380,7 +409,7 @@ namespace PstpFinder
       gmx_rmpbc_done(gpbc);
 
     output_env_done(oenv);
-    close_trj(status);
+    close_trx(status);
     gotTrajectory = false;
 
     if(bDGsol)
@@ -406,7 +435,7 @@ namespace PstpFinder
     if(not getTopology())
       gmx_fatal(FARGS, "Could not read topology file.\n");
 
-    if(not getTrajectory())
+    if(not gotTrajectory and not getTrajectory())
       gmx_fatal(FARGS, "Could not read coordinates from statusfile.\n");
 
     currentFrame = 0;
@@ -426,11 +455,11 @@ namespace PstpFinder
 
     npdbatoms = top.atoms.nr;
     snew(top.atoms.pdbinfo, npdbatoms);
-    copy_mat(box, pdbbox);
+    copy_mat(fr.box, pdbbox);
 
     sub_xcm(xtop, isize, index.data(), top.atoms.atom, xcm, FALSE);
     if(_usePBC)
-      gpbc = gmx_rmpbc_init(&top.idef, ePBC, natoms, box);
+      gpbc = gmx_rmpbc_init(&top.idef, ePBC, natoms, fr.box);
 
     count = 0;
     do
@@ -438,9 +467,9 @@ namespace PstpFinder
       if(abortFlag)
         break;
       if(_usePBC)
-        gmx_rmpbc(gpbc, natoms, box, x);
-      sub_xcm(x, isize, index.data(), top.atoms.atom, xcm, FALSE);
-      do_fit(natoms, w_rls, xtop, x);
+        gmx_rmpbc(gpbc, natoms, fr.box, fr.x);
+      sub_xcm(fr.x, isize, index.data(), top.atoms.atom, xcm, FALSE);
+      do_fit(natoms, w_rls, xtop, fr.x);
 
       if(abortFlag)
         break;
@@ -449,9 +478,9 @@ namespace PstpFinder
         atom_id aid = index[i];
         for(int d = 0; d < DIM; d++)
         {
-          xav[i * DIM + d] += x[aid][d];
+          xav[i * DIM + d] += fr.x[aid][d];
           for(int m = 0; m < DIM; m++)
-            U[i][d * DIM + m] += x[aid][d] * x[aid][m];
+            U[i][d * DIM + m] += fr.x[aid][d] * fr.x[aid][m];
         }
       }
 
@@ -619,6 +648,7 @@ namespace PstpFinder
 
     sfree(gnames);
     sfree(grps->index);
+    sfree(grps->a);
     sfree(grps);
     delete m_atoms;
 
@@ -632,8 +662,8 @@ namespace PstpFinder
     matrix topbox;
     char title[1024];
 
-#ifdef GMX45    
-    read_tpx(tprName.c_str(), &ir, box, &natoms, 0, 0, 0, &mtop);
+#ifdef GMX45
+    read_tpx(tprName.c_str(), &ir, fr.box, &natoms, 0, 0, 0, &mtop);
 #else
     read_tpx( tprName.c_str(), &step, &t, &lambda, &ir, box, &natoms,
         0, 0, 0, &mtop);
@@ -653,12 +683,15 @@ namespace PstpFinder
       snew(oenv, 1);
       output_env_init_default(oenv);
     }
+    else
+      close_trx(status);
 
     cachedNFrames = 0;
     timeStepCached = 0;
     currentFrame = 0;
 
-    if((natoms = read_first_x(oenv, &status, trjName.c_str(), &t, &x, box))
+    if((natoms = read_first_frame(oenv, &status, trjName.c_str(), &fr,
+                                  TRX_NEED_X))
        == 0)
 #else
     if((natoms =
@@ -690,7 +723,7 @@ namespace PstpFinder
     bool out;
 
 #ifdef GMX45
-    out = read_next_x(oenv, status, &t, natoms, x, box);
+    out = read_next_frame(oenv, status, &fr);
 #else
     out = read_next_x(status, &t, natoms, x, box);
 #endif

src/Gromacs.h
@@ -135,11 +135,10 @@ namespace PstpFinder
       int status, step;
       real lambda;
 #endif
-      real t;
-      rvec *x, *xtop;
-      matrix box;
+      rvec *xtop;
       int natoms, ePBC;
       t_topology top;
+      t_trxframe fr;
 
       gmx_atomprop_t aps;
       string trjName, tprName;

src/SasAnalysis.h
@@ -303,6 +303,9 @@ namespace PstpFinder
     Base::analysisThread->stop();
     delete Base::analysisThread;
 
+    for(auto& frame : Base::frames)
+      delete[] frame;
+
     delete Base::serializer;
   }
 
@@ -314,8 +317,15 @@ namespace PstpFinder
 
     if(Base::frames.size() != 0)
       flush();
+    Base::analysisThread->waitForFlush();
     Base::analysisThread->stop();
     delete Base::analysisThread;
+
+    for(auto& chunk : Base::chunks)
+    {
+      for(auto& frame : chunk)
+        delete[] frame;
+    }
 
     delete Base::serializer;
     Base::sasMetaStream.close();

src/SasAnalysisThread.h
@@ -218,6 +218,28 @@ namespace PstpFinder
       virtual ~SasAnalysisThread() {}
       virtual void
       threadSave() { Base::threadSave(); }
+
+      void waitForFlush()
+      {
+        if(Base::isStopped)
+          return;
+
+        Base::parent->bufferMutex.lock();
+        while(Base::parent->bufferCount < Base::parent->bufferMax and not Base::isStopped)
+        {
+          unique_lock<mutex> lock(Base::wakeMutex);
+          Base::parent->bufferMutex.unlock();
+          Base::wakeCondition.wait(lock);
+          Base::parent->bufferMutex.lock();
+
+          if(Base::parent->gromacs and Base::parent->gromacs->isAborting())
+          {
+            Base::parent->bufferMutex.unlock();
+            break;
+          }
+        }
+        Base::parent->bufferMutex.unlock();
+      }
   };
 }
 

commit c41096bc6bb52b1a9434184c0be0a230ab057fb0

Fixed major bug introduced previously
Bug introduced in commit b63d29a7e3f8953f3dbad28ec336b204a7547c1c because of the output of read_first_frame, which is a boolean and not the number of atoms read.

src/Gromacs.cpp
@@ -564,9 +564,8 @@ namespace PstpFinder
     timeStepCached = 0;
     currentFrame = 0;
 
-    if((natoms = read_first_frame(oenv, &status, trjName.c_str(), &fr,
+    if(not read_first_frame(oenv, &status, trjName.c_str(), &fr,
                                   TRX_NEED_X))
-       == 0)
 #else
     if((natoms =
             read_first_x(&status, trjName.c_str(), &t, &x, box)) == 0)
@@ -578,6 +577,9 @@ namespace PstpFinder
     }
     else
     {
+#ifdef GMX45
+      natoms = fr.natoms;
+#endif
       readyToGetX = true;
       return gotTrajectory = true;
     }
@@ -773,15 +775,13 @@ namespace PstpFinder
 
     output_env_t _oenv;
     t_trxstatus *_status;
-    int _natoms;
     t_trxframe _fr;
     float time1, time2;
 
     snew(_oenv, 1);
     output_env_init_default(_oenv);
 
-    if((_natoms = read_first_frame(_oenv, &_status, trjName.c_str(), &_fr, 0))
-       == 0)
+    if(not read_first_frame(_oenv, &_status, trjName.c_str(), &_fr, 0))
     {
       output_env_done(_oenv);
       return 0;
@@ -806,14 +806,12 @@ namespace PstpFinder
 
     output_env_t _oenv;
     t_trxstatus *_status;
-    int _natoms;
     t_trxframe _fr;
 
     snew(_oenv, 1);
     output_env_init_default(_oenv);
 
-    if((_natoms = read_first_frame(_oenv, &_status, trjName.c_str(), &_fr, 0))
-       == 0)
+    if(read_first_frame(_oenv, &_status, trjName.c_str(), &_fr, 0))
     {
       close_trx(_status);
       output_env_done(_oenv);